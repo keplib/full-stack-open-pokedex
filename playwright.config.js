5ea3419ce427fcc444b71ae54d1a51b582b15926
keplib <keplib@gmail.com>
2024-06-11 15:56:06 +0200

    part11.9 update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -75,5 +75,10 @@ module.exports = defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
-
